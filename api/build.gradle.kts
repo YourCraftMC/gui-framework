621908567a4c416b80fd996e75c75a324939634f
huanmeng-qwq <1871735932@qq.com>
2025-01-18 14:46:58 +0800

    build: Remove unused dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,5 +2,4 @@ dependencies {
     compileOnlyApi("org.jetbrains:annotations:26.0.0")
 
     compileOnlyApi("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
-    compileOnlyApi("com.github.retrooper:packetevents-api:2.7.0")
 }
